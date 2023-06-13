f62eefd70f0860492fd9a9859198e3ac16037a52
Oleks-Y <borith1111@gmail.com>
2023-06-13 14:21:16 +0200

    add tests for isJSON function to check if string, undefined, null, and number are not json

--- server/utils/__tests__/isJSON.test.ts
+++ server/utils/__tests__/isJSON.test.ts
@@ -8,4 +8,20 @@ describe('isJSON', () => {
   test('can detect non-json', () => {
     expect(isJSON('{"foo": "bar"')).toBe(false);
   });
+
+  test('string is not json', () => {
+    expect(isJSON('foo')).toBe(false);
+  });
+
+  test('undefined is not json', () => {
+    expect(isJSON(undefined)).toBe(false);
+  });
+
+  test('null is not json', () => {
+    expect(isJSON(null)).toBe(false);
+  });
+
+  test('number is not json', () => {
+    expect(isJSON(1)).toBe(false);
+  });
 });
